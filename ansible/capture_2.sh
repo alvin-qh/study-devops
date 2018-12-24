d2146c5f011dd8fab326dd23aada4fa0098db2cb
quhao <quhao@wisdomgarden.com>
2018-12-24 13:56:02 +0800

    add ansible demo

--- ansible/capture_2.sh
+++ ansible/capture_2.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 source .virtualenv/bin/activate
-ansible -i inventories/common vm -a "/bin/echo 'Hello'"
-ansible -i inventories/common vm -a "/bin/ls -alh /"
-ansible -i inventories/common vm -a "/bin/pwd"  -b --become-user root --ask-become-pass
+
+# run command on remote server
+ansible -i inventories/common vm -a "echo Hello World"
+
+# run command with sudo permission
+ansible -i inventories/common vm --become --become-user root --ask-become-pass -a "ls -alh /"
+ansible -i inventories/common vm --become --become-user root --ask-become-pass -a "touch aaa"
+
+# connect ssh with different users (not defined in `~/.ssh/config`)
+ansible -i inventories/common vm --user root --ask-pass -a "pwd"
