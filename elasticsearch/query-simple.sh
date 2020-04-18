a538652c24b8ae362376b0bafa761ed13451803e
Alvin.Qu <quhao317@163.com>
2020-04-18 23:51:44 +0800

    [es] create es curl demo

--- elasticsearch/query-simple.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-curl -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' \
-     -X GET 'http://localhost:9200/study/_search?pretty' -d '@json/query-simple.json';
-
-curl -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' \
-     -X GET 'http://localhost:9200/study/_search?pretty&q=intro:java';
