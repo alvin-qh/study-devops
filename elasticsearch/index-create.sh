ef4a7a36acba51f6f8075c018c82e5b386d8d7ff
quhao <quhao@wisdomgarden.com>
2020-04-22 15:40:18 +0800

    [es] index demo

--- elasticsearch/index-create.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-curl -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' \
-     -X PUT 'http://localhost:9200/study?pretty' -d '@json/create-index.json';
--- elasticsearch/index-get-settings.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-curl -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' \
-     -X GET 'http://localhost:9200/study/_settings?pretty';
-
-curl -H 'Cache-Control: no-cache' -H 'Content-Type: application/json' \
-     -X GET 'http://localhost:9200/study/_mappings?pretty';
