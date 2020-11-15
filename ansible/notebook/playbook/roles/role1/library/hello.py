01749f3a31708b44383a99dc495d834ea09d2b47
Alvin <quhao317@163.com>
2020-11-15 23:48:34 +0800

    [ansible] add role library demo

--- ansible/notebook/playbook/roles/role1/library/hello.py
+++ ansible/notebook/playbook/roles/role1/library/hello.py
@@ -1,6 +1,11 @@
 import os
 from ansible.module_utils.basic import (AnsibleModule)
 
+# Arguments template of module, use:
+# - name: "..."
+#   hello:
+#     name: Alvin   # argument 'name'
+#     gender: 'M'   # argument 'gender'
 arg_template = {
     'name': {
         'type': 'str',
@@ -12,7 +17,10 @@ arg_template = {
     }
 }
 
+# create ansible module, setup module argument
 module = AnsibleModule(argument_spec=arg_template)
+
+# get argument values
 arg_name = module.params['name']
 arg_gender = module.params['gender']
 
@@ -20,13 +28,11 @@ prefix = 'Mr.'
 if (arg_gender == 'F'):
     prefix = 'Ms.'
 
+# make output string
 msg = 'Hello {} {}'.format(prefix, arg_name)
 
+# run command on remote server
 os.system('echo {}'.format(msg))
 
-result = {
-    'changed': True,
-    'msg': msg
-}
-
-module.exit_json(**result)
+# exit module and report result
+module.exit_json(changed=True, msg=msg)
