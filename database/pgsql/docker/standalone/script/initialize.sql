5666dc26e257690e3929f6e05284e46887b62119
Alvin <quhao317@163.com>
2023-07-03 01:32:46 +0800

    [database] add pgsql documents

--- database/pgsql/docker/standalone/script/initialize.sql
+++ database/pgsql/docker/standalone/script/initialize.sql
@@ -1,5 +1,8 @@
-CREATE USER test;
+-- 创建用户 test, 具备登录权限
+CREATE ROLE test LOGIN ENCRYPTED PASSWORD 'test';
 
+-- 创建数据库 test
 CREATE DATABASE test;
 
+-- 将 test 数据库的全部权限赋予用户 test
 GRANT ALL PRIVILEGES ON DATABASE test TO test;
